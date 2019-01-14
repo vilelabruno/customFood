2c5c1d1dc2fae205d5b13ed8c1ff5276ae82eb5f
Bruno Vilela <a16042@bcc.unifal-mg.edu.br>
2019-01-14 21:10:39 -0200

    first commit

---
+++ src/customfodd/CustomFodd.java
@@ -0,0 +1,13 @@
+package customfodd;
+
+import java.time.DayOfWeek;
+
+public class CustomFodd {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        DayOfWeek d;
+    }
+    
+}
---
+++ src/customfodd/Regra.java
@@ -0,0 +1,13 @@
+package customfodd;
+
+import java.time.DayOfWeek;
+import java.util.Map;
+
+public class Regra {
+    
+    private int id;
+    
+    private boolean produtosCustomizaveis;
+    
+    private Map<DayOfWeek, Float> descontos;
+}
